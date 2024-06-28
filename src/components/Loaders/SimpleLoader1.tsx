117cecd25c8c1be4cc71bee63eda64f5cfac1c27
Colin Walker <thuner2007@outlook.com>
2024-06-28 10:35:15 +0200

    feat: add loader1

---
+++ src/components/Loaders/SimpleLoader1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SimpleLoader1.css';
+
+interface SimpleLoader1Prop {
+  
+}
+
+const SimpleLoader1: React.FC<SimpleLoader1Prop> = ({  }) => {
+  return (
+    <div className='mainDivSimpleLoader1'></div>
+  );
+};
+
+export default SimpleLoader1;
